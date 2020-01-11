82731225b5a8c6e24a2bfd22909f6d6312130beb
Brittney Cortez <brittneycortez@Brittneys-MacBook-Pro.local>
2020-01-10 23:23:51 -0800

    added song and artist contoller with rest actions

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,39 @@
+class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+  end
+  
+  def new
+   @song = Song.new    
+  end
+  
+  def create
+    @song = Song.new(song_params)
+    if @song.save
+      redirect_to(songs_path)
+    else
+      render(:new)
+    end
+  end
+  
+  def edit
+    @song = Song.find(params[:id]) 
+  end
+
+  def update 
+    @song = Song.find(params[:id])
+    if @song.update(song_params)
+      redirect_to(songs_path)
+    else 
+      render(:edit)
+    end
+  end
+  
+  def destroy
+    @song = Song.find(params[:id])
+    @song.destroy
+    redirect_to(songs_path)
+  end
+
+end
